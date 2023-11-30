a98ea72fce2ba1da7b7a7f173e0076e038d20ec0
Pratibha <pratipw23@gmail.com>
2023-11-30 21:07:47 +0530

    FP:Initial Commit

---
+++ src/functionalProgramming/PringAllNumbersInList.java
@@ -0,0 +1,22 @@
+package functionalProgramming;
+
+import java.util.List;
+
+public class PringAllNumbersInList {
+
+	public static void main(String[] args) {
+		
+		printAllNumbers(List.of(10,5,4,3,19,20));
+	}
+	
+	public static void print(int n) {
+		System.out.println(n);
+	}
+
+	private static void printAllNumbers(List<Integer> nums) {
+
+		nums.stream()
+			.forEach(PringAllNumbersInList::print);
+	}
+
+}
