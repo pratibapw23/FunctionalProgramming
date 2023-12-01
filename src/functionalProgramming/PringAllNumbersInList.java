f1b62d3842417e1ddd1b7be6ddc4d798af6a5783
Pratibha <pratipw23@gmail.com>
2023-12-01 11:56:34 +0530

    FP:Print all event numbers in list using streams API and Lambda expressions

--- src/functionalProgramming/PringAllNumbersInList.java
+++ src/functionalProgramming/PringAllNumbersInList.java
@@ -6,7 +6,8 @@ public class PringAllNumbersInList {
 
 	public static void main(String[] args) {
 		
-		printAllNumbers(List.of(10,5,4,3,19,20));
+//		printAllNumbers(List.of(10,5,4,3,19,20));
+		printEvenNumbers(List.of(10,5,4,3,19,20,11));
 	}
 	
 //	public static void print(int n) {
@@ -16,6 +17,11 @@ public class PringAllNumbersInList {
 	private static void printAllNumbers(List<Integer> nums) {
 
 		nums.stream()
+			.forEach(System.out::println);
+	}
+	private static void printEvenNumbers(List<Integer> nums) {
+		nums.stream()
+			.filter(number -> number%2==0)
 			.forEach(System.out::println);
 	}
 
