fb7e081a9b76151542e2fa8c1962bc43ed19a378
Pratibha <pratipw23@gmail.com>
2023-12-03 20:39:19 +0530

    FP:added functions to print length of each character in list and characters having length 4

--- src/functionalProgramming/PrintAllIndividualCoursesInList.java
+++ src/functionalProgramming/PrintAllIndividualCoursesInList.java
@@ -6,8 +6,10 @@ public class PrintAllIndividualCoursesInList {
 
 	public static void main(String[] args) {
 		
-		List<String> courses=List.of("Spring","Spring Boot", "API", "Microservices","AWS");
-		individualCourses(courses);
+		List<String> courses=List.of("Spring","Spring Boot", "API", "Microservices","AWS","APID");
+//		individualCourses(courses);
+//		printWordContainingFourCharacters(courses);
+		printLengthOfEachCharacter(courses);
 
 	}
 
@@ -16,6 +18,16 @@ public class PrintAllIndividualCoursesInList {
 		courses.stream()
 			.filter(course -> course.contains("Spring"))
 			.forEach(System.out::println);
+	}
+	private static void printWordContainingFourCharacters(List<String> courses) {
+		courses.stream()
+		.filter(course -> course.length()==4)
+		.forEach(System.out::println);
+	}
+	private static void printLengthOfEachCharacter(List<String> courses) {
+		courses.stream()
+		.map(course -> course + " "+ course.length())
+		.forEach(System.out::println);
 	}
 
 }
